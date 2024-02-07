913b1a0a52ac9949fdc52476db899aa4824928e2
tleblanc302 <62121950+tleblanc302@users.noreply.github.com>
2024-02-07 15:50:07 -0600

    Add files via upload

---
+++ gen-pass.py
@@ -0,0 +1,8 @@
+## a python script which uses python3-bcrypt to prompt for a password and hashes it.
+
+import getpass
+import bcrypt
+
+password = getpass.getpass("password: ")
+hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+print(hashed_password.decode())
